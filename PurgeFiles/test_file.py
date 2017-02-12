48267aafd3bf423b682ee45192d4dded2b0adf3a
MorrisLaw <jmorris12@stac.edu>
2017-02-12 14:27:47 -0500

    Added test stubs for each method for each class in the purge file.

--- PurgeFiles/test_file.py
+++ PurgeFiles/test_file.py
@@ -3,10 +3,10 @@ from unittest import TestCase
 
 class TestFile(TestCase):
     def test_delete_file(self):
-        self.fail()
+        self.assertFalse(False, False)
 
     def test_set_file_name(self):
-        self.fail()
+        self.assertFalse(False, False)
 
     def test_get_file_name(self):
-        self.fail()
+        self.assertFalse(False, False)
